1e802b1697420a6a2fea8e952e02e73cfa185717
acadavid <acadavid@gmail.com>
2013-04-26 11:51:10 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bidsystem::Application.config.secret_token = '0c42a57426ff566511883853d722d90d636b5b06ea2a2fefe7b938fc4a58f3b2921d003f97e52844f9890d8dfdac86ebdabcbf2ae74fad2c4dd7a989f5aea133'
