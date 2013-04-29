88c74cc6387b270cef6076afbc7907ce35b41351
acadavid <acadavid@gmail.com>
2013-04-29 18:58:59 -0500

    Removed some unnecesary methods from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,6 @@ class UsersController < ApplicationController
 
   respond_to :json
 
-  def index
-    @users = User.all
-
-    respond_with @users
-  end
-
-  def show
-    @user = User.find(params[:id])
-
-    respond_with @user
-  end
-
-  def new
-    @user = User.new
-
-    respond_with @user
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def create
     @user = User.new(params[:user])
 
